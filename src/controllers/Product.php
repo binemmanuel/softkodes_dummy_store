6094b9bef69a5cd2de5e2403622ebd80b5fb6c49
Bin Emmanuel <53215111+binemmanuel@users.noreply.github.com>
2023-03-10 11:56:52 +0100

    Added product photo upload functionality

--- src/controllers/Product.php
+++ src/controllers/Product.php
@@ -11,6 +11,8 @@ use Binemmanuel\ServeMyPhp\{
 
 use Model\{
     Product as ProductModel,
+    CoverPhoto,
+    File,
 };
 
 class Product extends BaseController
@@ -43,10 +45,97 @@ class Product extends BaseController
 
         $products = $product->findAll(['productId' => $product->productId]);
 
-        return $response::sendJson(
-            $products,
-            allowMethods: 'GET',
-            allowHeader: 'Authorization, Content-Type, x-auth-token',
-        );
+        return $response::sendJson($products);
+    }
+
+    public function add(Request $request, Response $response)
+    {
+
+
+        // return $response::sendJson($request->jsonBody());
+        $product = (new ProductModel($this->db))->loadData($request->jsonBody());
+
+        $product->makeRules([
+            'name' => [Rule::REQUIRED],
+            'price' => [Rule::REQUIRED, Rule::NUMBER],
+            'image' => [Rule::REQUIRED],
+            'description' => [Rule::REQUIRED, [Rule::MIN_LENGTH, 5]],
+        ]);
+
+        if ($product->hasError()) {
+            return $response::sendJson([
+                'error' => true,
+                'errors' => $product->errors(),
+            ], statusCode: 400);
+        }
+
+        $product = $product->save();
+
+        return $response::sendJson($product);
+    }
+
+    public function addProductImage(Request $request, Response $response)
+    {
+        $file = $request->file[0] ?? [];
+        $uploadedBy = $request->uploadedBy ?? '';
+
+        if (empty($file)) {
+            return $response->sendJson([
+                'error' => true,
+                'message' => 'No file selected for upload',
+            ], statusCode: 400);
+        }
+
+        if (empty($uploadedBy)) {
+            return $response->sendJson([
+                'error' => true,
+                'type' => 'uploadedBy',
+                'message' => 'This is required field',
+            ], statusCode: 400);
+        }
+
+        // Check if the file a valid one
+        if (!File::isValid(pathinfo($file['name'], PATHINFO_EXTENSION))) {
+            return $response->sendJson([
+                'error' => true,
+                'message' => 'Invalid file type',
+                'file' => $file,
+            ], statusCode: 400);
+
+            // Check if the file has a valid size
+        } else if ($file['size'] === 0) {
+            return $response->sendJson([
+                'error' => true,
+                'message' => 'File too large or not valid',
+                'file' => $file,
+            ], statusCode: 400);
+        }
+
+        $file = (new File($this->db))->uploadFile($file, uploadedBy: $uploadedBy);
+
+        $coverPhoto = (new CoverPhoto($this->db))->loadData([
+            'url' => $file['url'] ?? '',
+            'uploadedBy' => $uploadedBy,
+        ]);
+
+        $hasCover = $coverPhoto->find(['uploadedBy' => $uploadedBy]);
+
+        if (!empty($hasCover)) {
+            $coverPhoto->update(['uploadedBy' => $uploadedBy]);
+
+            return $response->sendJson([
+                'error' => false,
+                'file' => $file,
+                'message' => 'Updated successfully',
+            ]);
+        }
+
+        $coverPhoto->save();
+
+        return $response->sendJson([
+            'error' => false,
+            'file' => $file,
+            'message' => 'Uploaded successfully',
+        ]);
     }
 }
