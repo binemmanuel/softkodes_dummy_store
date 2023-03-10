95e7aba971f452b329dea03b6a97b5aa97a51d9e
Bin Emmanuel <53215111+binemmanuel@users.noreply.github.com>
2023-03-10 11:56:25 +0100

    Created a test endpoint

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,8 @@ require __DIR__ . '/../src/config.php';
 
 use Binemmanuel\ServeMyPhp\{
     Database,
+    Request,
+    Response,
     Router,
 };
 use Controller\{
@@ -10,14 +12,25 @@ use Controller\{
     Product
 };
 
+use RebaseData\Converter\Converter;
+use RebaseData\InputFile\InputFile;
+
 
 $database = (new Database($_ENV))->mysqli();
 
 $router = new Router($database);
 
-$router->get('/', [Home::class, 'index']);
+// $router->get('/', [Home::class, 'index']);
 
 $router->get('/products/get/all', [Product::class, 'getAll']);
 $router->get('/products/get', [Product::class, 'get']);
+$router->post( '/products/add', [Product::class, 'add']);
+$router->post('/products/upload/cover', [Product::class, 'addProductImage']);
+
+// addProductImage
+
+$router->get('/',  function (Request $request, Response $response) {
+    return $response::sendJson(['message' => 'Dev Test API'])
+});
 
 $router->run();
